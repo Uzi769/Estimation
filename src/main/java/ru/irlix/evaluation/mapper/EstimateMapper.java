984df8c52d36035083fc9521cb43c6402f23da3d
Kiseleva Alena <kiseleva_elena92@mail.ru>
2021-08-11 17:20:52 +0400

    edited mapper for estimate convertation

--- src/main/java/ru/irlix/evaluation/mapper/EstimateMapper.java
+++ src/main/java/ru/irlix/evaluation/mapper/EstimateMapper.java
@@ -6,6 +6,8 @@ import ru.irlix.evaluation.dao.entity.Phase;
 import ru.irlix.evaluation.dto.EstimateDTO;
 import ru.irlix.evaluation.dto.PhaseDTO;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EstimateMapper {
     Estimate estimateDtoToEstimate(EstimateDTO estimateDto);
@@ -13,4 +15,8 @@ public interface EstimateMapper {
 
     PhaseDTO phaseToPhaseDto(Phase phase);
     Phase phaseDtoToPhase(PhaseDTO phaseDTO);
+
+    List<PhaseDTO> phasesToPhasesDto (List<Phase> phases);
+    List<Phase> phasesDtoToPhases (List<PhaseDTO> phasesDTO);
+
 }
--- src/main/java/ru/irlix/evaluation/mapper/EstimateMapperIml.java
+++ src/main/java/ru/irlix/evaluation/mapper/EstimateMapperIml.java
@@ -6,6 +6,9 @@ import ru.irlix.evaluation.dao.entity.Phase;
 import ru.irlix.evaluation.dto.EstimateDTO;
 import ru.irlix.evaluation.dto.PhaseDTO;
 
+import java.time.Instant;
+import java.util.List;
+
 @Component
 public class EstimateMapperIml implements EstimateMapper{
     @Override
@@ -15,6 +18,15 @@ public class EstimateMapperIml implements EstimateMapper{
             return null;
         }
         Estimate estimate = new Estimate();
+        estimate.setId(estimateDto.getId());
+        estimate.setName(estimateDto.getName());
+        estimate.setCreateDate(Instant.parse(estimateDto.getCreateDate()));
+        estimate.setDescription(estimateDto.getDescription());
+        estimate.setRisk(estimateDto.getRisk());
+        estimate.setStatus(estimateDto.getStatus());
+        estimate.setClient(estimateDto.getClient());
+        estimate.setCreator(estimateDto.getCreator());
+        estimate.setPhases(phasesDtoToPhases(estimateDto.getPhases()));
         return estimate;
     }
 
@@ -25,6 +37,15 @@ public class EstimateMapperIml implements EstimateMapper{
             return null;
         }
         EstimateDTO estimateDTO = new EstimateDTO();
+        estimateDTO.setId(estimate.getId());
+        estimateDTO.setName(estimate.getName());
+        estimateDTO.setCreateDate(estimate.getCreateDate().toString());
+        estimateDTO.setDescription(estimate.getDescription());
+        estimateDTO.setRisk(estimate.getRisk());
+        estimateDTO.setStatus(estimate.getStatus());
+        estimateDTO.setClient(estimate.getClient());
+        estimateDTO.setCreator(estimate.getCreator());
+        estimateDTO.setPhases(phasesToPhasesDto(estimate.getPhases()));
         return estimateDTO;
     }
 
@@ -46,5 +67,15 @@ public class EstimateMapperIml implements EstimateMapper{
         }
         Phase phase = new Phase();
         return phase;
+    }
+
+    @Override
+    public List<PhaseDTO> phasesToPhasesDto(List<Phase> phases) {
+        return null;
+    }
+
+    @Override
+    public List<Phase> phasesDtoToPhases(List<PhaseDTO> phasesDTO) {
+        return null;
     }
 }
