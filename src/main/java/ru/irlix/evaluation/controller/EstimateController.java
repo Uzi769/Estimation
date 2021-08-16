a55a4f7a52ef1d1736476cc8707d717db20fbc00
Kiseleva Alena <kiseleva_elena92@mail.ru>
2021-08-16 12:11:12 +0400

    added save phases into service and mapper

--- src/main/java/ru/irlix/evaluation/controller/EstimateController.java
+++ src/main/java/ru/irlix/evaluation/controller/EstimateController.java
@@ -5,7 +5,6 @@ import ru.irlix.evaluation.dto.EstimateDTO;
 import ru.irlix.evaluation.service.EstimateService;
 import ru.irlix.evaluation.utils.Utils;
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
