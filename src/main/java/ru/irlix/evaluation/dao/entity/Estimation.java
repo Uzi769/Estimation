b949b49f9650e7a55a1e50373d0b6a0b153eb807
mechttatel <88131058+mechttatel@users.noreply.github.com>
2021-08-17 09:44:19 +0400

    merging

--- src/main/java/ru/irlix/evaluation/dao/entity/Estimation.java
+++ src/main/java/ru/irlix/evaluation/dao/entity/Estimation.java
@@ -6,15 +6,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
-import javax.persistence.Column;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import java.time.Instant;
 import java.util.List;
