95c31e89170aaa75da157abf24a25b26a31ed81b
mechttatel <88131058+mechttatel@users.noreply.github.com>
2021-08-31 16:34:50 +0400

    path env variable

--- src/main/java/ru/irlix/evaluation/utils/report/ReportHelper.java
+++ src/main/java/ru/irlix/evaluation/utils/report/ReportHelper.java
@@ -1,8 +1,6 @@
 package ru.irlix.evaluation.utils.report;
 
-import lombok.RequiredArgsConstructor;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import ru.irlix.evaluation.dao.entity.Estimation;
@@ -10,15 +8,15 @@ import ru.irlix.evaluation.dto.request.ReportRequest;
 import ru.irlix.evaluation.utils.report.sheet.EstimationWithDetailsSheet;
 import ru.irlix.evaluation.utils.report.sheet.Sheet;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class ReportHelper {
+
+    @Value("${document-path}")
+    private String path;
 
     public Resource getEstimationReportResource(Estimation estimation, ReportRequest request) throws IOException {
         ExcelWorkbook excelWorkbook = new ExcelWorkbook();
@@ -27,6 +25,6 @@ public class ReportHelper {
         sheets.add(new EstimationWithDetailsSheet(excelWorkbook));
         sheets.forEach(s -> s.getSheet(estimation, request));
 
-        return excelWorkbook.save("C:/output/estimations.xlsx");
+        return excelWorkbook.save(path);
     }
 }
