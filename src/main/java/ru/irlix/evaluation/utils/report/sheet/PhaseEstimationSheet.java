afd599b4d101d5f0d929e1e6c2caafe4a6faba81
Kiseleva Alena <kiseleva_elena92@mail.ru>
2021-09-01 14:39:55 +0400

    fix 4 sheet

--- src/main/java/ru/irlix/evaluation/utils/report/sheet/PhaseEstimationSheet.java
+++ src/main/java/ru/irlix/evaluation/utils/report/sheet/PhaseEstimationSheet.java
@@ -5,72 +5,67 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import ru.irlix.evaluation.dao.entity.Estimation;
 import ru.irlix.evaluation.dao.entity.Phase;
-import ru.irlix.evaluation.dao.entity.Task;
 import ru.irlix.evaluation.dto.request.ReportRequest;
 import ru.irlix.evaluation.utils.report.ExcelWorkbook;
+import ru.irlix.evaluation.utils.report.ReportMath;
 
 @RequiredArgsConstructor
 public class PhaseEstimationSheet implements Sheet {
 
     private final ExcelWorkbook helper;
-
+    private XSSFSheet sheet;
     private int rowNum = 0;
+    private final short ROW_HEIGHT = 380;
+    private double hoursMaxSummary;
+    private double costMaxSummary;
 
     @Override
     public void getSheet(Estimation estimation, ReportRequest request) {
-        XSSFSheet sheet = helper.getWorkbook().createSheet("Оценка по фазам");
+        sheet = helper.getWorkbook().createSheet("Оценка по фазам");
 
-        sheet.setColumnWidth(0, 8000);
-        sheet.setColumnWidth(1, 5000);
-        sheet.setColumnWidth(2, 6000);
+        configure();
+        fillHeader();
 
-        Row row = sheet.createRow(rowNum);
-        row.setHeight((short) 1050);
+        for (Phase phase : estimation.getPhases()) {
+            Row row = createRow(ROW_HEIGHT);
+            helper.setPhaseCell(row, phase.getName(), 0);
+
+            double sumHoursMax = ReportMath.calcPhaseSummaryMaxHours(phase, request);
+            hoursMaxSummary += sumHoursMax;
+            helper.setPhaseCell(row, sumHoursMax, 1);
+
+            double sumCostMax = ReportMath.calcPhaseSummaryMaxCost(phase, request);
+            costMaxSummary += sumCostMax;
+            helper.setPhaseCell(row, sumCostMax, 2);
+        }
+        fillSummary();
+    }
+
+    private void fillSummary() {
+        Row row = createRow(ROW_HEIGHT);
+        helper.setPhaseCell(row, "Итого по проекту:", 0);
+        helper.setPhaseCell(row, hoursMaxSummary, 1);
+        helper.setPhaseCell(row, costMaxSummary, 2);
+    }
+
+    private void fillHeader() {
+        final short HEADER_ROW_HEIGHT = 1050;
+        Row row = createRow(HEADER_ROW_HEIGHT);
         helper.setHeaderCell(row, "Задачи", 0);
         helper.setHeaderCell(row, "Часы (мин)", 1);
         helper.setHeaderCell(row, "Стоимость, RUB", 2);
-
-        for (Phase phase : estimation.getPhases()) {
-            rowNum++;
-            row = sheet.createRow(rowNum);
-            helper.setPhaseCell(row, phase.getName(), 0);
-
-            double sumHoursMax = phase.getTasks().stream()
-                    .mapToDouble(t -> !t.getTasks().isEmpty()
-                            ? t.getTasks().stream().mapToDouble(nt -> nt.getHoursMax() * 60).sum()
-                            : t.getHoursMax() * 60)
-                    .sum();
-
-            helper.setPhaseCell(row, sumHoursMax, 1);
-
-            double costMax = phase.getTasks().stream()
-                    .mapToDouble(t -> !t.getTasks().isEmpty()
-                            ? t.getTasks().stream().mapToDouble(nt -> nt.getHoursMax() * getCost(request, nt)).sum()
-                            : t.getHoursMax() * getCost(request, t)
-
-                    ).sum();
-
-            helper.setPhaseCell(row, costMax, 2);
-        }
-
     }
 
-    private double getCost(ReportRequest request, Task t) {
-        double roleCost = 0;
-        switch (t.getRole().getDisplayValue()) {
-            case "Аналитик":
-                roleCost = request.getAnalystCost();
-                break;
-            case "Дизайнер":
-                roleCost = request.getDesignCost();
-                break;
-            case "Тестировщик":
-                roleCost = request.getQaCost();
-                break;
-            case "Разработчик":
-                roleCost = request.getDevCost();
-                break;
-        }
-        return roleCost;
+    private Row createRow(short height) {
+        Row row = sheet.createRow(rowNum);
+        row.setHeight(height);
+        rowNum++;
+        return row;
+    }
+
+    private void configure() {
+        sheet.setColumnWidth(0, 8000);
+        sheet.setColumnWidth(1, 5000);
+        sheet.setColumnWidth(2, 6000);
     }
 }
