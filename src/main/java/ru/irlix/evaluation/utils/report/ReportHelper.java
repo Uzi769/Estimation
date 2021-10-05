fd3816b3a91e6c21f96fd11df3d91bf9591a3dff
mechttatel <88131058+mechttatel@users.noreply.github.com>
2021-10-05 09:40:27 +0400

    Remove report naming

--- src/main/java/ru/irlix/evaluation/utils/report/ReportHelper.java
+++ src/main/java/ru/irlix/evaluation/utils/report/ReportHelper.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import ru.irlix.evaluation.dao.entity.Estimation;
-import ru.irlix.evaluation.utils.localization.MessageBundle;
 import ru.irlix.evaluation.utils.report.sheet.*;
 
 import java.io.IOException;
@@ -12,15 +11,12 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.ResourceBundle;
 
 @Component
 public class ReportHelper {
 
     @Value("${document-path}")
     private String path;
-
-    private final ResourceBundle messageBundle = MessageBundle.getMessageBundle();
 
     public Resource getEstimationReportResource(Estimation estimation, Map<String, String> request) throws IOException {
         List<String> roleCosts = EstimationReportSheet.getRoleCosts(estimation, request);
@@ -38,19 +34,7 @@ public class ReportHelper {
 
         sheets.forEach(s -> s.getSheet(estimation, request));
 
-        String filePath = Paths.get(path, getFileName(estimation)).toString();
+        String filePath = Paths.get(path, "report.xls").toString();
         return excelWorkbook.save(filePath);
-    }
-
-    private String getFileName(Estimation estimation) {
-        String client = estimation.getClient() == null
-                ? messageBundle.getString("string.client")
-                : estimation.getClient();
-
-        String name = estimation.getName() == null
-                ? messageBundle.getString("string.name")
-                : estimation.getName();
-
-        return messageBundle.getString("string.estimation") + " " + client + " " + name + ".xls";
     }
 }
