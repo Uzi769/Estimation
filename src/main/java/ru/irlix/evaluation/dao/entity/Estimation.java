d232dc9e4aa015ab88df1f700921445c71d75d65
Kiseleva Alena <kiseleva_elena92@mail.ru>
2021-10-12 13:20:59 +0400

    refactor

--- src/main/java/ru/irlix/evaluation/dao/entity/Estimation.java
+++ src/main/java/ru/irlix/evaluation/dao/entity/Estimation.java
@@ -2,7 +2,6 @@ package ru.irlix.evaluation.dao.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -13,7 +12,6 @@ import java.util.List;
 @Table(name = "estimation")
 @Getter
 @Setter
-@ToString
 @NamedEntityGraph(
         name = "estimation.phases",
         attributeNodes = @NamedAttributeNode("phases")
@@ -40,7 +38,6 @@ public class Estimation {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "status")
-    @ToString.Exclude
     private Status status;
 
     @Column(name = "client")
@@ -51,7 +48,6 @@ public class Estimation {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "estimation")
     @OrderBy("sortOrder ASC")
-    @ToString.Exclude
     private List<Phase> phases;
 
     @ManyToMany
@@ -59,11 +55,9 @@ public class Estimation {
             name = "user_estimation",
             joinColumns = @JoinColumn(name = "estimation_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id"))
-    @ToString.Exclude
     private List<User> users;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "estimation")
-    @ToString.Exclude
     private List<FileStorage> fileStorages;
 
     @PrePersist
--- src/main/java/ru/irlix/evaluation/dao/helper/FolderHelper.java
+++ src/main/java/ru/irlix/evaluation/dao/helper/FolderHelper.java
@@ -9,7 +9,6 @@ import ru.irlix.evaluation.dao.entity.Folder;
 import ru.irlix.evaluation.exception.NotFoundException;
 import ru.irlix.evaluation.repository.FolderRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Log4j2
@@ -20,17 +19,12 @@ public class FolderHelper {
 
     private final FolderRepository folderRepository;
 
-    private static final String[] DOCUMENTS_LIST = {
-            "word", "docx", "doc", "txt", "pdf", "xls", "doc", "xml", "rtf", "zip", "7z", "rar"
-    };
-
-    private static final String[] PICTURES_LIST = {
-            "png", "jpeg", "jpg", "svg", "gif", "bmp"
-    };
-
-    private static final String[] AUDIO_LIST = {
-            "mid", "mp3", "wav"
-    };
+    private static final List<String> docList
+            = List.of("word", "docx", "doc", "txt", "pdf", "xls", "doc", "xml", "rtf", "zip", "7z", "rar");
+    private static final List<String> picList
+            = List.of("png", "jpeg", "jpg", "svg", "gif", "bmp");
+    private static final List<String> audioList
+            = List.of("mid", "mp3", "wav");
 
     public Folder findRoleById(Long id) {
         return folderRepository.findById(id)
@@ -39,11 +33,11 @@ public class FolderHelper {
 
     public Folder getFolder(String fileName) {
         String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-        if (Arrays.asList(DOCUMENTS_LIST).contains(extension))
+        if (docList.contains(extension))
             return folderRepository.findByValue("documents");
-        if (Arrays.asList(PICTURES_LIST).contains(extension))
+        if (picList.contains(extension))
             return folderRepository.findByValue("pictures");
-        if (Arrays.asList(AUDIO_LIST).contains(extension))
+        if (audioList.contains(extension))
             return folderRepository.findByValue("audio");
         return folderRepository.findByValue("other");
     }
