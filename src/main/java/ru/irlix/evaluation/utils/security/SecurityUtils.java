d013df646b80c52365b518bcb8133f6091c7c36e
ivan.kalachikov <ivan.kalachikov@irlix.ru>
2021-10-13 14:59:02 +0400

    fix logging of creation

--- src/main/java/ru/irlix/evaluation/utils/security/SecurityUtils.java
+++ src/main/java/ru/irlix/evaluation/utils/security/SecurityUtils.java
@@ -3,6 +3,7 @@ package ru.irlix.evaluation.utils.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 public class SecurityUtils {
     public static boolean hasAccessToAllEstimations() {
@@ -10,5 +11,16 @@ public class SecurityUtils {
         return authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .anyMatch(a -> a.equals("ROLE_ADMIN") || a.equals("ROLE_SALES"));
+    }
+
+    public static String getUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt principal = (Jwt) authentication.getPrincipal();
+        return String.valueOf(principal.getClaims().get("name"));
+    }
+
+    public static String getKeycloakId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 }
