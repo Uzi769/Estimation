7946648ee156274c3541f38ff330f34e25c94961
dima.dolovin <dima.golovin@irlix.ru>
2021-10-21 07:32:10 +0300

    review typeDoc

--- src/main/java/ru/irlix/evaluation/dao/mapper/FolderMapper.java
+++ src/main/java/ru/irlix/evaluation/dao/mapper/FolderMapper.java
@@ -2,26 +2,29 @@ package ru.irlix.evaluation.dao.mapper;
 
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import ru.irlix.evaluation.dao.entity.Folder;
 import ru.irlix.evaluation.dto.response.FolderResponse;
 
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public abstract class FolderMapper {
 
-    @Mapping(target = "typeDoc", ignore = true)
     public abstract FolderResponse folderToFolderResponse(Folder folder);
 
     @AfterMapping
     protected void map(@MappingTarget FolderResponse folderResponse, Folder folder) {
-        folderResponse.setTypeDoc(folder.getFileStorageList()
+        folderResponse.setTypeDoc(getTypes(folder));
+    }
+
+    public Set<String> getTypes(Folder folder){
+        return folder.getFileStorageList()
                 .stream()
                 .map(type -> type.getFileName()
                         .substring(type.getFileName().lastIndexOf("."))
                         .toLowerCase())
-                .collect(Collectors.toSet()));
+                .collect(Collectors.toSet());
     }
 }
